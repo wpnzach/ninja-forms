a255220973912d504ff37491f3765812295b822a
Kenny Hall <klhall1987@gmail.com>
2018-02-16 11:38:25 -0500

    added a check for the key word on in value of columns on the submissions page.

--- includes/Fields/Checkbox.php
+++ includes/Fields/Checkbox.php
@@ -81,14 +81,14 @@ class NF_Fields_Checkbox extends NF_Abstracts_Input
         // If the field type is equal to checkbox...
         if( 'checkbox' == $field->get_setting( 'type' ) ) {
             // Backwards compatibility check for the new checked value setting.
-            if( null == $field->get_setting( 'checked_value' ) && 1 == $value ) {
+            if( null == $field->get_setting( 'checked_value' ) && 1 == $value || 'on' == $value ) {
                 return __( 'Checked', 'ninja-forms' );
             } elseif( null == $field->get_setting( 'unchecked_value' ) && 0 == $value ) {
                 return __( 'Unchecked', 'ninja-forms');
             }
 
             // If the field value is set to 1....
-            if( 1 == $value ) {
+            if( 1 == $value || 'on' == $value) {
                 // Set the value to the checked value setting.
                 $value = $field->get_setting( 'checked_value' );
             } else {
