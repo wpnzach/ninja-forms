fc117913a531795067295512520ba32d890c99a1
Eric Windham <eric.windham@gmail.com>
2018-02-16 11:21:49 -0500

    Fixes Star Rating issues - Issue#2815 (#3370)
    * made some changes to the star rating fields
    
    * fixes default value and required setting for star rating field

--- assets/js/front-end/controllers/fieldStarRating.js
+++ assets/js/front-end/controllers/fieldStarRating.js
@@ -12,15 +12,22 @@ define([], function() {
 
         initRating: function( view ){
             jQuery( view.el ).find( '.starrating' ).rating();
+
         },
 
         renderRatings: function() {
-
         	var html = document.createElement( 'span' );
-        	for (var i = 0; i <= this.default - 1; i++) {
+        	// changed from 'default' to 'number_of_stars'
+        	for (var i = 0; i <= this.number_of_stars - 1; i++) {
                 var template = nfRadio.channel( 'app' ).request( 'get:template',  '#tmpl-nf-field-starrating-star' );
                 var num = i + 1;
-                var htmlFragment = template( { id: this.id, classes: this.classes, num: num } );
+                var checked = '';
+
+                // Check to see if current 'star' matches the default value
+		        if ( this.value == num ) {
+		        	checked = 'checked';
+		        }
+                var htmlFragment = template( { id: this.id, classes: this.classes, num: num, checked: checked } );
                 html.appendChild(
                     document.createRange().createContextualFragment( htmlFragment )
                 );
